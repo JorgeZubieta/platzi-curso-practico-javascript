993cf9940d8d8739fb4199afb82768d649e750a5
jorge zubieta <zubietajor@gmail.com>
2021-07-16 16:43:38 -0300

    2nd taller completado - descuento

--- descuentos.js
+++ descuentos.js
@@ -1,22 +1,20 @@
-// // // definimos las vriables precio original y porcentage de descuento
-// // definimos las variables precio original y porcentage de descuento
-// const precioOriginal = 120;
-// const descuento = 18;
-
-// const porcentajePrecioConDescuento = 100 - descuento;
-// const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
-
-// console.log({
-// 	precioOriginal,
-// 	descuento,
-// 	porcentajePrecioConDescuento,
-// 	precioConDescuento,
-// });
-
-
 // Ahora lo realizamos con funciones para el ingreso de parametros!
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
+}
+
+function clickDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    // no usamos alert, mostramos el prcio con html!
+    const R = document.getElementById("ResultP");
+    R.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
