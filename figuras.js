8e55a5559d6a33911531f8905e9a4fc35012eaac
jorge zubieta <zubietajor@gmail.com>
2021-07-15 23:56:30 -0300

    reto completado, triangulo, circulo y triangulo isosceles

--- figuras.js
+++ figuras.js
@@ -1,94 +1,17 @@
 // Formulas del cuadrado
 console.group("Formulas - Cuadrados");
-    // const ladoCuadrado = 5; - lo ingresaremos desde consola
-    // console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
-
-    // Volvemos const en funcion
-    // const perimetroCuadrado = ladoCuadrado * 4;
     function perimetroCuadrado(lado) {
         return lado * 4;
     }
-    // console.log("Perimetro del cuadrado: " + perimetroCuadrado + "cm");
-
-
-    // const areaCuadrado = ladoCuadrado * ladoCuadrado;
     function areaCuadrado (lado) {
         return lado * lado;
     }
-    // console.log("Area cuadrado: " + areaCuadrado + "cm^2");
-console.groupEnd();
-
-// Formulas del Triangulo
-console.group("Formulas - Triangulos"); 
-    // const ladoTriangulo1 =6;
-    // const ladoTriangulo2 =6;
-    // const baseTriangulo =4;
-
-    // console.log(
-    //     "Los lados del triangulo miden: " 
-    //     + ladoTriangulo1 + "cm, "
-    //     + ladoTriangulo2 + "cm, "
-    //     + baseTriangulo + "cm."
-    // );
-
-    // const alturaTraingulo = 5.5;
-    // console.log("La altura del triangulo es: " + alturaTraingulo + "cm");
-
-
-    // const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-    function perimetroTriangulo(lado1, lado2, base) {
-        return lado1 + lado2 + base;
-    };
-    // console.log("Perimetro del triangulo es: " + perimetroTriangulo + "cm");
-
-    // const areaTriangulo = (baseTriangulo * alturaTraingulo) /2;
-    function areaTriangulo(base, altura) {
-        return (base * altura)/2;
-    }
-    // console.log("Area triangulo: " + areaTriangulo + "cm^2");
-console.groupEnd();
-
-// Formulas del Circulo
-console.group("Formulas - Circulo"); 
-    // RADIO
-    // const radioCirculo =4;
-    // console.log( "El Radio del Circclo mide: " + radioCirculo + "cm");   
-    
-    // DIAMETRO
-    // const diametroCirculo = radioCirculo*2;
-    function diametroCirculo(radio) {
-        return radio * 2;
-    }
-    // console.log( "El Diametro del Ciruclo mide: " + diametroCirculo+ "cm");    
-    
-    // PI
-    // 2 formas de definir el valor PI
-    // 1 - manualmente    let PI = 3.1415;
-    // 2 - utilizando una herramienta de js la cual tiene varias herramientas como asi el valor PI!
-    let PI = Math.PI;
-    // console.log( "El valor pi es de: " + PI);
-
-    // CIRCUNFERENCIA / PERIMETRO
-    // const perimetroCiruclo = diametroCirculo*PI;
-    function perimetroCirculo(radio) {
-        const diametro = diametroCirculo(radio);
-        return diametro * PI;
-        // NOTA : aprendimos a llamar a otras funciones dentro de una funcion!
-    }
-    // console.log("Perimetro del Circulo es: " + perimetroCiruclo + "cm");
-
-    // AREA
-    // const areaCirculo = (radioCirculo * radioCirculo) * PI;
-    function areaCirculo(radio){
-        return (radio * radio) * PI;
-    } 
-    // console.log("El area de Circulo es: " + areaCirculo + "cm^2");
 console.groupEnd();
 
 // Aqui interactuaremos con el HTML llamando a las funciones!
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado");
-    let value = input.value;
+    const value = input.value;
     // llamamos a la funcion  perimetroCuadrado la cual va arealizar el calculo!
     const perimetro = perimetroCuadrado(value);
     // nos mostrara en una ventana de alerta el resultado!
@@ -101,5 +24,101 @@ function calcularAreaCuadrado() {
     // llamaos a la funcion areaCuadrado
     const area = areaCuadrado(value);
     // nos mostrara en una ventana de alerta el resultado!
+    alert(area);
+}
+
+
+// Formulas del Triangulo
+console.group("Formulas - Triangulos"); 
+    function perimetroTriangulo(lado1, lado2, base) {
+        return lado1 + lado2 + base;
+    };
+    
+    function areaTriangulo(base, altura) {
+        return (base * altura)/2;
+    }
+    function alturaTrianguloIsosceles(lado1, lado2, base) {
+            return (Math.sqrt((lado1 * lado2) + (base * base)));
+    }
+console.groupEnd();
+
+// Aqui interactuaremos con el HTML llamando a las funciones!
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("InputTrianguloLado1");
+    const lado1 = Number(input1.value);
+
+    const input2 = document.getElementById("InputTrianguloLado2");
+    const lado2 = Number(input2.value);
+    
+    const input3 = document.getElementById("InputTrianguloBase");
+    const base = Number(input3.value);
+    
+    // llamamos a la funcion  perimetroCuadrado la cual va arealizar el calculo!
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    alert('El perimetro Total es de: ' + perimetro + 'cm');
+}
+
+function calcularAreaTriangulo() {
+    const inputAltura = document.getElementById("InputTrianguloAltura");
+    const altura = Number(inputAltura.value);
+    
+    const inputBase = document.getElementById("InputTrianguloBase");
+    const base = Number(inputBase.value);
+    
+    // llamamos a la funcion  perimetroCuadrado la cual va arealizar el calculo!
+    const area = areaTriangulo(altura, base);
+    alert('El Arear Total es de: ' + area + 'cm^2 (centimetros cuadrados)');
+}
+
+function calcularAlturaTrianguloIsosceles(){
+    const input1 = document.getElementById("InputTrianguloLado1");
+    const lado1 = Number(input1.value);
+
+    const input2 = document.getElementById("InputTrianguloLado2");
+    const lado2 = Number(input2.value);
+    
+    const input3 = document.getElementById("InputTrianguloBase");
+    const base = Number(input3.value);
+    
+    if (lado1 === lado2) {
+        // llamamos a la funcion  perimetroCuadrado la cual va arealizar el calculo
+        const altura = alturaTrianguloIsosceles(lado1, lado2, base);
+        alert('La altura del Traingulo Isosceles es de: ' + altura + 'cm');
+    }else{
+        alert('Lad1 1 y Lado 2 deben ser iguales!');
+    }
+
+}
+
+
+// Formulas del Circulo
+console.group("Formulas - Circulo"); 
+    function diametroCirculo(radio) {
+        return radio * 2;
+    }
+    // PERIMETRO (circunferencia)
+    let PI = Math.PI;
+    function perimetroCirculo(radio) {
+        const diametro = diametroCirculo(radio);
+        return diametro * PI;
+    }
+    // AREA
+    function areaCirculo(radio){
+        return (radio * radio) * PI;
+    } 
+console.groupEnd();
+
+// Aqui interactuaremos con el HTML llamando a las funciones!
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const area = areaCirculo(value);
     alert(area);
 }
