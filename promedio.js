506bfa593e78e7b903678604b8327b8984faf02f
jorge zubieta <zubietajor@gmail.com>
2021-07-19 00:16:57 -0300

    calulo de media aritmetica con una funcion y un for

--- promedio.js
+++ promedio.js
@@ -1,18 +1,15 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
-// recordemos que en un array la primera poscision es 0 luego 1 y asi!
+// la funcion recibira la lista que decidamos por consola
+function calcularMediaAritmetica (lista){
 
-let sumaLista1 = 0;
-// length : se refiere a la cantidad totoal de elementos de un array (cuantos casillas tiene)
-for (let i = 0; i < lista1.length; i++) {
-// con este for decimos ue se va acumplir solo si "i" (contador) es menor que la cantidad de elementos del array, si lo supera no se cumple.
-    sumaLista1 = sumaLista1 + lista1[i];
-    // [i] con esta forma tomamos el contenido (valor) del arrary vinculado con el contador i (poscisiion).
+    let sumaLista = 0;
+    // length : se refiere a la cantidad totoal de elementos de un array (cuantos casillas tiene)
+    for (let i = 0; i < lista.length; i++) {
+    // con este for decimos ue se va acumplir solo si "i" (contador) es menor que la cantidad de elementos del array, si lo supera no se cumple.
+        sumaLista = sumaLista + lista[i];
+        // [i] con esta forma tomamos el contenido (valor) del arrary vinculado con el contador i (poscisiion).
+        
+        const promediolista = sumaLista / lista.length;
+        return promediolista;
+    }
+
 }
-
-// en este punto finaliza el for con la suma total y el primedio de el array! 
-const promediolista1 = sumaLista1 / lista1.length;
