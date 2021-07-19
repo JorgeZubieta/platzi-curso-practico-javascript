e337a4f7c05733278295f29f58a37dad28fd8149
jorge zubieta <zubietajor@gmail.com>
2021-07-19 01:38:10 -0300

    metodo reduce

--- promedio.js
+++ promedio.js
@@ -1,15 +1,13 @@
 // la funcion recibira la lista que decidamos por consola
 function calcularMediaAritmetica (lista){
-
-    let sumaLista = 0;
-    // length : se refiere a la cantidad totoal de elementos de un array (cuantos casillas tiene)
-    for (let i = 0; i < lista.length; i++) {
-    // con este for decimos ue se va acumplir solo si "i" (contador) es menor que la cantidad de elementos del array, si lo supera no se cumple.
-        sumaLista = sumaLista + lista[i];
-        // [i] con esta forma tomamos el contenido (valor) del arrary vinculado con el contador i (poscisiion).
-        
-        const promediolista = sumaLista / lista.length;
-        return promediolista;
-    }
-
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    const sumaLista = lista.reduce (
+        function (valorAcumulado = 0, valorArray) {
+            return valorAcumulado = valorAcumulado + valorArray;
+        }
+    );    
+    const promediolista = sumaLista / lista.length;
+    return promediolista;
 }
