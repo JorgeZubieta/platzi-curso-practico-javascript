60edbb4ddc4b0a6594d32c30dd97d107650c1ceb
jorge zubieta <zubietajor@gmail.com>
2021-07-24 09:39:18 -0300

    reto moda con funciones completado

--- moda.js
+++ moda.js
@@ -1,45 +1,59 @@
-const lista1 = [
-    1,
-    90,
-    1,
-    2,
-    4,
-    1,
-    4,
-    51,
-    4,
-    7,
-    8,
-    90,
-    1,
-    1,
-];
+// Puedo poner este desde le html dentro de las etiqueta script
+function clickGuardar(){
+    // sueldoCapturado no lo definimos cono var o const o let porque asi se autodefine como variable global que vive en todo el proyecto
+    numeroCapturado = document.getElementById("InputNumero").value;
+    console.log(numeroCapturado);
+    add();
+}
 
-// declaramos una lista de objetos vacia
+var baseDatos = [];
+// luego deberiamos realizar una funcion para que guarde el sueldo caprturado
+
+function add(){
+    console.log("funca");
+    baseDatos.push(numeroCapturado);
+    console.log(baseDatos);
+};
+
+    // declaramos una lista de objetos vacia
 const lista1Count = {};
 
-// map recorre un array
-// Cuenta de cuantoas veces aparece un elemento dentro del array1
-lista1.map(
-    function(elemento){
-        // si existe el elemento se suma uno al contador!
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else {
-        // si no existe que ponga el contador en 1
-        lista1Count[elemento] = 1;
+// suamos los elementos del array para sacar la mediana
+function clickModa() {
+    baseDatos.sort(function(prev, next) {
+        return prev - next;
+    });
+    
+    // map recorre un array
+    // Cuenta de cuantas veces aparece un elemento dentro del array1
+    baseDatos.map(
+        function(elemento){
+            // si existe el elemento se suma uno al contador!
+            if (lista1Count[elemento]) {
+                lista1Count[elemento] += 1;
+            } else {
+            // si no existe que ponga el contador en 1
+            lista1Count[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
-// Object.entries convierte un objeto en un array
-// Sort oredna un array conn dos parametros
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        // [1] apunta a la segunda pos del arrray pra ordenerlo
-        return elementoA[1] - elementoB[1];
-    }
-);
-// entramos a la ultima pos asi que usamos length
-// 5 elementos de 0 a 4 restamos para que de 5 elementos se le reste al contador una pos!
-const moda = lista1Array[lista1Array.length - 1];
+    // Object.entries convierte un objeto en un array
+    // Sort oredna un array conn dos parametros
+
+    lista1Array = Object.entries(lista1Count).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];
+        }
+    );
+
+    // entramos a la ultima pos asi que usamos length
+    moda = lista1Array[lista1Array.length - 1];
+    
+    console.log(baseDatos);
+    // mostramos el resultado por pantalla
+    resultado = document.getElementById("resultModa");
+    resultado.innerText = "El numero que se repite mas veces es: " + moda[0] + "y se repitio: " + moda[1];
+}
+
+
